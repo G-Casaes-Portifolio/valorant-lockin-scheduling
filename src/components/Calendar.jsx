162ddf6a54b4c0409375cba7999cb4c39b43cf66
Zokyn <gustavo.casaes@mail.uft.edu.br>
2023-02-25 06:58:51 -0300

    adicionado informações sobre resultados de partida finalizadas

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,17 @@
+import data from "../data/games.json"
+
 function Calendar() {
-  
+  return (
+    <div>
+      {data.games.map((game) => (
+        <tr key={game.id}>
+          {game.teams.map((team) => (
+            <td>{team}</td>
+          ))}
+        </tr>
+      ))}
+    </div>
+  )
 }
 
 export default Calendar;
