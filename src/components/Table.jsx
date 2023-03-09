56fd45f2e4764c8d0cb1a9876cb7b42815d6eda0
Zokyn <gustavo.casaes@mail.uft.edu.br>
2023-03-09 20:38:35 -0300

    formatação para eliminar o scroll em medias grandes

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,49 +1,47 @@
 import data from "../data/games2.json"
 
+/* function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Horario</th>
+        <th>Times</th>
+        <th>Resultado</th>
+      </tr>
+    </thead>
+  )
+} */
+
 export default function Table() {
   const options = {weekday: 'long', month: 'numeric', day: 'numeric',
     hour: '2-digit', minute: '2-digit'}
 
   return(
     <div id="table-view" className="panel">
-      <table>
-        <thead>
-          <tr>
-            <th>Horario</th>
-            <th>Times</th>
-            <th>Resultado</th>
-          </tr>
-        </thead>
-        <tbody>
+      <div className="table-container">
         {data.games.map((game, id) => {
           const siglaParticipant1 = game.participant1.split(' ', 1)
           const siglaParticipant2 = game.participant2.split(' ', 1)
           const placar = game.finalScore.split(":")
-          if(placar[0] > placar[1]){
+          /*if(placar[0] > placar[1]){
             let winner = placar[0];
-          }
+          } */
           return(
-            <tr key={id}>
-              <td id="date">
+            <div className="table-row" key={id}>
+              <div className="table-cell" id="date">
                 {new Date(game.datetime).toLocaleDateString(
                   'pt-BR', options).toString()}
-              </td>
-              <td id="participants">
+              </div>
+              <div className="table-cell" id="participants">
                 {siglaParticipant1} x {siglaParticipant2}
-              </td>
-              <td id="score">
+              </div>
+              <div className="table-cell" id="score">
                 {game.finalScore}
-              </td>
-            </tr>
+              </div>
+            </div>
           )
-        })}
-        </tbody>
-      </table>
+          })}
+      </div>
     </div>
-
-
   )
-  /*   return(
-    console.log(data.games)
-  ) */
 }
